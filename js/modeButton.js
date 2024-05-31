1eee867c1b92900be9d655114db6fd29e1731db7
ashborn3 <114130969+ashborn3@users.noreply.github.com>
2024-05-31 18:25:32 +0530

    updated the listener to apply card css rules

--- js/modeButton.js
+++ js/modeButton.js
@@ -1,14 +1,19 @@
-// modeButton.js
-
 document.getElementById('theme-toggle').addEventListener('click', function () {
     const body = document.body;
     const navbar = document.querySelector('.navbar');
     const themeIcon = document.getElementById('theme-icon');
+    const cards = document.querySelectorAll('.card');
 
     body.classList.toggle('dark-mode');
     body.classList.toggle('light-mode');
     navbar.classList.toggle('navbar-dark-mode');
     navbar.classList.toggle('navbar-light-mode');
+
+    // Toggle mode for each card
+    cards.forEach(card => {
+        card.classList.toggle('dark-mode');
+        card.classList.toggle('light-mode');
+    });
 
     // Change the icon based on the current mode
     if (body.classList.contains('dark-mode')) {
